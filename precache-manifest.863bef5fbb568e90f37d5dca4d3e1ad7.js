38fdb0c79f6782e221fae7b79fc58797a3a30042
Maxim Kozin <maxa.kozin@gmail.com>
2020-07-28 17:22:16 +0300

    Updates

---
+++ precache-manifest.863bef5fbb568e90f37d5dca4d3e1ad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d0acf59e873eb566279ecdcc00f424",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "7bc995de594d303279c3",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.48258e5a.chunk.css"
+  },
+  {
+    "revision": "ab670db7f1467ba661e0",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.0840c6cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.0840c6cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bc995de594d303279c3",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.b345e596.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
